97b16a5332720e7e89dcd63a84ab1e6a34aac12f
Jacky <964413011@qq.com>
2021-08-02 08:57:19 +0800

    减少InfoTag标签识别中的分支嵌套

--- src/anaylze/syntax/literal/info_tag/mod.rs
+++ src/anaylze/syntax/literal/info_tag/mod.rs
@@ -1,16 +1,12 @@
 mod at;
 mod img;
 
-use crate::anaylze::{
-    lexical::{LexicalType, OutDataLoader},
-    syntax::{
+use crate::anaylze::{SignTableHandle, lexical::{LexicalType, OutDataLoader, util::check_next_sign}, syntax::{
         literal::{structs::Image},
         LoadErr, LoadStatus, SyntaxLoadNext,
-    },
-    SignTableHandle,
-};
+    }};
 
-use super::{structs::At, InfoTag};
+use super::{InfoTag, structs::At, util::check_tag_name};
 
 impl<'a, S> SyntaxLoadNext<'a, OutDataLoader<'a, S>, LexicalType> for InfoTag
 where
@@ -20,16 +16,12 @@ where
         last: LexicalType,
         expr: &mut OutDataLoader<'a, S>,
     ) -> Result<LoadStatus<Self, LexicalType>, LoadErr> {
-        let res = Image::load_next(last, expr)?;
-        if res.is_ok() {
-            return Ok(LoadStatus::ok(Self::Img(res.unwrap())));
-        } else {
-            let res = At::load_next(res.unwarp_unmatch(), expr)?;
-            if res.is_ok() {
-                return Ok(LoadStatus::ok(Self::At(res.unwrap())));
-            } else {
-                return Ok(LoadStatus::unmatch(res.unwarp_unmatch()));
-            }
-        }
+    if let Some(_)=check_tag_name(&last, "img", true){
+        Ok(Image::load_next(last, expr)?.and_then(|img|Self::Img(img)))
+    }else if let Some(_)=check_tag_name(&last, "at", true) {
+        Ok(At::load_next(last, expr)?.and_then(|at|Self::At(at)))
+    }else {
+        Ok(LoadStatus::unmatch(last))
+    }
     }
 }
